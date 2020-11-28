88ac01a190dea37b9d2bf4b3e60bec2904ebf124
anr <anr@ableton.com>
2020-11-28 19:34:36 +0100

    Print the first twenty prime numbers

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,48 @@
 
 // https://www.w3schools.com/cpp/cpp_for_loop.asp
 
+// printNumber is a function which takes an int, called 'number' as a parameter.
+// The return type is a boolean, or bool, which is either true or false.
+bool divides(int smallNumber, int bigNumber)
+{
+  return (bigNumber % smallNumber) == 0;
+}
+
+
+// The function called 'isPrime' will return a boolean, or bool,
+// which has the value true or false
+bool isPrime(int number)
+{
+  for (int divisor = 2; divisor < number; divisor++)
+  {
+    if (divides(divisor, number))
+    {
+      // a smaller number divides 'number' so it is not prime
+      return false;
+    }
+  }
+
+  // we couldn't find a smaller number to divide number
+  return true;
+}
+
 
 int main(int argc, const char * argv[])
 {
-  int bigNumber = 10;
+  int largestPrimeToFind = 20;
   
-  // The for loop initializes 'int number = 1;'
-  // Each loop it tests if 'number < bigNumber', if so it keeps on going
-  // and each time it goes round it, it does 'number = number + 1'
-  for (int number = 1; number <= bigNumber; number = number + 1)
+  // The for loop initializes 'int number = 2;'
+  // During each loop, the if statement checks if the 'number' is prime
+  for (int number = 2; number <= largestPrimeToFind; number++)
   {
-    std::cout << "printing " << number << "\n";
+    if (isPrime(number))
+    {
+      std::cout << number << " is prime\n";
+    }
+    else
+    {
+      std::cout << number << " is not prime\n";
+    }
   }
   
   return 0;
