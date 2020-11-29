9f0b1bebb00ff3a91ed3e562b7120644a7eafafc
anr <anr@ableton.com>
2020-11-29 13:05:42 +0100

    Store our primes in a vector

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,11 @@
 // cmath is a 'library' that adds in mathematics functions
 #include <cmath>
 #include <iostream>
+// vector is part of the standard library and stores a sequence of elements
+#include <vector>
 
 // https://en.cppreference.com/w/cpp/numeric/math/sqrt
+// https://en.cppreference.com/w/cpp/container/vector
 
 // printNumber is a function which takes an int, called 'number' as a parameter.
 // The return type is a boolean, or bool, which is either true or false.
@@ -35,22 +38,42 @@ bool isPrime(int number)
 }
 
 
-int main(int argc, const char * argv[])
+// The function 'findPrimes' returns a std::vector of primes
+// \param largestPrimeToFind is the largest prime that will
+// be held in the vector
+std::vector<int> findPrimes(int largestPrimeToFind)
 {
-  int largestPrimeToFind = 20;
-  
-  // The for loop initializes 'int number = 2;'
-  // During each loop, the if statement checks if the 'number' is prime
+  // The vector is called 'primes' and holds types 'int'
+  // here it is empty. Although we don't know how many primes
+  // the vector needs to hold, it can expand. This is called
+  // dynamic memory as it can change size.
+  std::vector<int> primes;
+
   for (int number = 2; number <= largestPrimeToFind; number++)
   {
     if (isPrime(number))
     {
-      std::cout << number << " is prime\n";
+      // push_back is a function that adds an element to the 'vector'
+      primes.push_back(number);
     }
-    else
-    {
-      std::cout << number << " is not prime\n";
-    }
+  }
+  
+  return primes;
+}
+
+
+int main(int argc, const char * argv[])
+{
+  int largestPrimeToFind = 20;
+  std::vector<int> primes = findPrimes(largestPrimeToFind);
+  
+  // Type 'size_t' is a positive 'int'
+  for (size_t index = 0; index < primes.size(); index++)
+  {
+    // the vector has a series of elements. The elements are accessed
+    // by the square brackets. primes[0] is the first element, primes[1]
+    // is the second element, etc.
+    std::cout << primes[index] << " is prime\n";
   }
   
   return 0;
